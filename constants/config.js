f0b8775015d4fffd5e1784d1f12674e9a2060afb
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-02 23:23:17 +0530

    cors update

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,21 @@
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-  ],
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      process.env.CLIENT_URL,
+    ];
+
+    if (
+      allowedOrigins.includes(origin) ||
+      !origin ||
+      process.env.ALLOW_ALL_ORIGINS === "true"
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
